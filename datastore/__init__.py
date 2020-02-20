1b8bce567e3472f30754a5ad5a8f77a98f958714
Aaron Young <ayoung3210@gmail.com>
2020-02-20 12:01:16 -0500

    Turned datastore into a package.

---
+++ datastore/__init__.py
@@ -0,0 +1,8 @@
+from .bunch import Bunch
+
+from .datastore import read_or_new_pickle
+from .datastore import save_pickle
+from .datastore import read_or_new_json
+from .datastore import save_json
+from .datastore import add_unique_postfix
+from .datastore import read_all_data_from_folder
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datastore-ayoung48",
+    version="0.0.1",
+    author="Aaron Young",
+    author_email="ayoung48@vols.utk.edu",
+    description="Useful datastore.",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/Geekdude/bunch/src/master/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6',
+)
