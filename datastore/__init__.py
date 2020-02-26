c6b4dfa749551302ba609d554ddec1fa6e783da1
Aaron Young <ayoung3210@gmail.com>
2020-02-26 17:03:33 -0500

    Added save_txt to init.

--- datastore/__init__.py
+++ datastore/__init__.py
@@ -2,6 +2,8 @@ from .bunch import Bunch
 
 from .datastore import read_or_new_pickle
 from .datastore import save_pickle
+from .datastore import read_or_new_txt
+from .datastore import save_txt
 from .datastore import read_or_new_json
 from .datastore import save_json
 from .datastore import add_unique_postfix
